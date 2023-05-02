90444e36efcbc421dc302da92c9a8b21f6d3ab39
Agnes Triselia Yudia <77945541+Agnesty@users.noreply.github.com>
2023-05-02 13:06:11 +0700

    Create ListView.swift

---
+++ Views/ListView.swift
@@ -0,0 +1,59 @@
+import SwiftUI
+
+struct ListView: View {
+    
+    @EnvironmentObject var listViewModel: ListViewModel
+//    @State var items: [ItemModel] = [
+//        ItemModel(title: "This is the first title!", isCompleted: false),
+//        ItemModel(title: "This is the second", isCompleted: true),
+//        ItemModel(title: "Third", isCompleted: false)
+//    ]
+    
+    var body: some View {
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) {
+                        item in 
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
+                    }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
+            }
+        }
+        .navigationTitle("Todo List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing: NavigationLink("Add", destination: AddView())
+        )
+    }
+    
+//    func deleteItem(indexSet: IndexSet) {
+//        items.remove(atOffsets: indexSet)
+//    }
+//    
+//    func moveItem(from: IndexSet, to: Int) {
+//        items.move(fromOffsets: from, toOffset: to)
+//    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+        ListView()    
+        }
+        .environmentObject(ListViewModel())
+    }
+}
+
+
